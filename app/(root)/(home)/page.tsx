b37f79028ad95b6f4145efa7a04092865b2aa234
Seenivasan Balakrishnan <connecttoseeni@gmail.com>
2023-10-16 05:16:02 +0530

    Lint fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,6 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">Hi</h1>
       <UserButton afterSignOutUrl="/" />
     </div>
   );
