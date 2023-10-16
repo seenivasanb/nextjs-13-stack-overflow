2d6df9ac8a5c7b526d730b86c304f6e3c34b2471
Seenivasan Balakrishnan <connecttoseeni@gmail.com>
2023-10-16 10:31:52 +0530

    Implementated NavBar

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import { useTheme } from "@/context/theme-provider";
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   const { mode } = useTheme();
 
   return (
     <div>
       <h2 className="h2-bold">{mode}</h2>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
