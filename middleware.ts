e422ba7a0f692920742ab749e45d9b71271c7e4f
Seenivasan Balakrishnan <connecttoseeni@gmail.com>
2023-11-01 22:15:53 +0530

    Added API webhook to public routes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,17 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/ask-question"],
+  publicRoutes: [
+    "/",
+    "/ask-question",
+    "/api/webhook",
+    "/tags",
+    "/tags/:id",
+    "/profile/:id",
+    "/community",
+    "/jobs",
+  ],
+  ignoredRoutes: ["/api/webhook", "api/chatgpt"],
 });
 
 export const config = {
